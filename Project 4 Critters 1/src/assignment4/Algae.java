b0990b8ec81ba64af133d804e7c0ff54f260b93f
Brent Atchison <atchisonbrent@gmail.com>
2016-10-01 13:54:04 -0500

    Readded project 4

---
+++ Project 4 Critters 1/src/assignment4/Algae.java
@@ -0,0 +1,29 @@
+/* 
+ * CRITTERS Algae.java
+ * EE422C Project 4 submission by
+ * Brent Atchison
+ * bma862
+ * 16455
+ * Dhruv Mathew
+ * dkm989
+ * 16455
+ * Slip days used: <0>
+ * Fall 2016
+ */
+
+package assignment4;
+/*
+ * Do not change this file.
+ */
+import assignment4.Critter.TestCritter;
+
+public class Algae extends TestCritter {
+
+	public String toString() { return "@"; }
+	
+	public boolean fight(String not_used) { return false; }
+	
+	public void doTimeStep() {
+		setEnergy(getEnergy() + Params.photosynthesis_energy_amount);
+	}
+}
---
+++ Project 4 Critters 1/src/assignment4/InvalidCritterException.java
@@ -0,0 +1,27 @@
+/* 
+ * CRITTERS InvalidCritterException.java
+ * EE422C Project 4 submission by
+ * Brent Atchison
+ * bma862
+ * 16455
+ * Dhruv Mathew
+ * dkm989
+ * 16455
+ * Slip days used: <0>
+ * Fall 2016
+ */
+
+package assignment4;
+
+public class InvalidCritterException extends Exception {
+	String offending_class;
+	
+	public InvalidCritterException(String critter_class_name) {
+		offending_class = critter_class_name;
+	}
+	
+	public String toString() {
+		return "Invalid Critter Class: " + offending_class;
+	}
+
+}
