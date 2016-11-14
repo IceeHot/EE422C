c3ffca8c82435b3ef98894d5c3b36fec1fef87eb
Brent Atchison <atchisonbrent@gmail.com>
2016-11-14 02:57:13 -0600

    Started work

--- Project 6 Multithreading/src/assignment6/BookingClient.java
+++ Project 6 Multithreading/src/assignment6/BookingClient.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Multithreading BookingClient.java
  * EE422C Project 6 submission by
  * Brent Atchison
@@ -12,19 +12,21 @@ package assignment6;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+
 import java.lang.Thread;
 
 public class BookingClient {
 	
-	static Theater theater;
-	static Map<String, Integer> office;
-	
+	static int client = 1;
+	private Theater t;
+	private Map<String, Integer> o;
+
 	/**
 	 * Initializes offices and theater like so:
 	 * office: {BX1 = 3, BX2 = 4, BX3 = 3, BX4 = 3, BX5 = 3}
-	 * theater: {3 rows, 5 seats per row, show: “Ouija"}
+	 * theater: {3 rows, 5 seats per row, show: ï¿½Ouija"}
 	 * Calls simulate()
-	 * 
+	 *
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -38,27 +40,57 @@ public class BookingClient {
 		BookingClient book = new BookingClient(office, theater);
 		book.simulate();
 	}
-	
+
 	/**
 	 * @param office maps box office ID to number of customers in line
 	 * @param theater the theater where the show is playing
 	 */
 	public BookingClient(Map<String, Integer> office, Theater theater) {
-		// TODO: Implement this constructor
-		office = office;
-		theater = theater;
+		o = office;
+		t = theater;
 	}
-	
+
+	public Theater getTheater() { return t; }
+
+	public Map<String, Integer> getOffice() { return o; }
+
 	/**
 	 * Starts the box office simulation by creating (and starting) threads
 	 * for each box office to sell tickets for the given theater
-	 * 
+	 *
 	 * @return list of threads used in the simulation,
 	 * should have as many threads as there are box offices
 	 */
 	public List<Thread> simulate() {
 		//TODO: Implement this method
 		List<Thread> threadList = new ArrayList<Thread>();
+		for (String key : this.getOffice().keySet()) {
+			Show me = new Show(this, key, this.getOffice().get(key));
+			Thread t = new Thread(me, key);
+			threadList.add(t);
+			t.start();
+		}
 		return threadList;
+	}
+
+	class Show implements Runnable {
+		
+		private BookingClient book;
+		private String boxOffice;
+		private int numCustomers;
+
+		public Show(BookingClient newBook, String box, int customers) {
+			book = newBook;
+			boxOffice = box;
+			numCustomers = customers;
+		}
+
+		public String getOffice() { return boxOffice; }
+
+		public int getCustomers() { return numCustomers; }
+
+		public void run() {
+			book.getTheater().printTicket(boxOffice, book.getTheater().bestAvailableSeat(), client++);
+		}
 	}
 }
--- Project 6 Multithreading/src/assignment6/Theater.java
+++ Project 6 Multithreading/src/assignment6/Theater.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Multithreading Theater.java
  * EE422C Project 6 submission by
  * Brent Atchison
@@ -13,11 +13,12 @@ package assignment6;
 import java.util.*;
 
 public class Theater {
-	
+
 	private int rows;
 	private int seats;
 	private String show;
-	
+	static List<Ticket> log = new ArrayList<Ticket>();
+
 	/**
 	 * Represents a seat in the theater
 	 * A1, A2, A3, ... B1, B2, B3 ...
@@ -38,7 +39,7 @@ public class Theater {
 		@Override
 		public String toString() { return toLetters(this.getRowNum()) + seatNum; }
 	}
-	
+
 	/**
 	 * Represents a ticket purchased by a client
 	 */
@@ -65,68 +66,103 @@ public class Theater {
 
 		@Override
 		public String toString() {
-			// TODO: Implement this method to return a string that resembles a ticket
-			return "Not yet implemented";
+
+			/* Ticket lines */
+			String edge = "-";
+			String line1 = "| Show: " + this.getShow();
+			String line2 = "| Box Office ID: " + this.getBoxOfficeId();
+			String line3 = "| Seat: " + this.getSeat().toString();
+			String line4 = "| Client: " + this.getClient();
+
+			/* Find necessary ticket length */
+			int tLength = line1.length() + 1;
+			if (line2.length() > tLength) { tLength = line2.length() + 1; }
+			if (line3.length() > tLength) { tLength = line3.length() + 1; }
+			if (line4.length() > tLength) { tLength = line4.length() + 1; }
+
+			/* Extend tickets to proper length */
+			for (int i = 0; i < tLength; i++) { edge += "-"; }
+			for (int i = line1.length(); i < tLength; i++) { line1 += " "; }
+			for (int i = line2.length(); i < tLength; i++) { line2 += " "; }
+			for (int i = line3.length(); i < tLength; i++) { line3 += " "; }
+			for (int i = line4.length(); i < tLength; i++) { line4 += " "; }
+
+			/* End ticket correctly */
+			edge += "\n";
+			line1 += "|\n";
+			line2 += "|\n";
+			line3 += "|\n";
+			line4 += "|\n";
+
+			return edge + line1 + line2 + line3 + line4 + edge;
 		}
 	}
-	
+
 	/**
 	 * Constructs a new theater
-	 * 
+	 *
 	 * @param numRows is number of rows
 	 * @param seatsPerRow is number of seats per row
 	 * @param show is the name of the show
 	 */
 	public Theater(int numRows, int seatsPerRow, String show) {
-		// TODO: Implement this constructor
 		this.rows = numRows;
 		this.seats = seatsPerRow;
 		this.show = show;
 	}
-	
+
 	public int getRows() { return rows; }
-	
+
 	public int getSeats() { return seats; }
-	
+
 	public String getShow() { return show; }
-	
+
 	/**
 	 * Calculates the best seat not yet reserved
-	 * 
+	 *
 	 * @return the best seat or null if theater is full
 	 */
 	public Seat bestAvailableSeat() {
-		//TODO: Implement this method
+		for (int i = 0; i < this.getRows(); i++) {
+			for (int j = 0; j < this.getSeats(); j++) {
+				if (log.isEmpty()) { return new Seat(i, j); }
+				for (Ticket t : log) {
+					Seat seat = new Seat(i, j);
+					if (t.getSeat().toString().equals(seat.toString())) { continue; }
+					else { return seat; }
+				}
+			}
+		}
 		return null;
 	}
-	
+
 	/**
 	 * Prints a ticket for the client after they reserve a seat
 	 * Also prints the ticket to the console
-	 * 
+	 *
 	 * @param boxOfficeId that sold the ticket
 	 * @param seat a particular seat in the theater
 	 * @param client who bought the ticket
 	 * @return a ticket or null if a box office failed to reserve the seat
 	 */
 	public Ticket printTicket(String boxOfficeId, Seat seat, int client) {
-		//TODO: Implement this method
-		return null;
+		if (seat == null) { return null; }
+		Ticket ticket = new Ticket(this.getShow(), boxOfficeId, seat, client);
+		System.out.println(ticket.toString());
+		log.add(ticket);
+		return ticket;
 	}
-	
+
 	/**
 	 * Lists all tickets sold for this theater in order of purchase
-	 * 
+	 *
 	 * @return list of tickets sold
 	 */
-	public List<Ticket> getTransactionLog() {
-		//TODO: Implement this method
-		return null;
-	}
-	
+	public List<Ticket> getTransactionLog() { return log; }
+
 	/**
 	 * Convert from base 10 to base 26
-	 * 
+	 *
 	 * @param num number to convert
 	 * @return string representation
 	 */
